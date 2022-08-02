e31782721386c62b09ae7d5a487c48800e77b399
MuriloEduardo <muriloeduardoooooo@gmail.com>
2022-08-02 16:16:10 -0300

    Adjusts talks body front

--- resources/views/livewire/talks-body.blade.php
+++ resources/views/livewire/talks-body.blade.php
@@ -1,22 +1,19 @@
 <div>
-    @if ($talk->completed_at && $talk->post->completed_at)
-    <div class="text-center text-gray-400">Essa conversa foi finalizada!</div>
-
-    @else
+    @if (!$talk->completed_at && !$talk->post->completed_at)
 
     @if ($talk->post->user_id === auth()->id())
     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6 border-b border-gray-200">
         <h4 class="text-xl text-green-600"><strong>Opa!</strong> {{ $talk->user->name }} <strong>disse que sabe te ajudar!</strong></h4>
         <p>Bora trocar uma ideia?</p>
-        <small>Sejam objetivos nas conversas e saibem que qualquer coisa tudo estará seguro e transparente para ambos.</small>
+        <small>Sejam objetivos nas conversas e saibam que qualquer coisa tudo estará seguro e transparente para ambos.</small>
     </div>
 
-    <div class="my-4 flex items-center">
+    <div class="flex items-center">
         @can ('accept-talk', $talk)
         <livewire:talks-accept :talk="$talk" />
         @endcan
 
-        @if ($talk->post->user->balance < $talk->post->amount)
+        @if ($talk->post->user->balance < $talk->post->amount && !$talk->post->accepted)
             <div class="px-3">
                 <span>Seu saldo é insuficiente, você precisa </span>
                 <a href="{{ route('transactions.create') }}" class="text-indigo-500">adicionar saldo</a>
@@ -29,9 +26,9 @@
     @endif
 
     @can ('conclude-talk', $talk)
-    <div>
+    <div class="my-3">
         @if ($talk->user_id === auth()->id())
-        <div class="mb-4 bg-white shadow-sm sm:rounded-lg p-4 border-b border-gray-200">
+        <div class="mb-3 bg-white shadow-sm sm:rounded-lg p-4 border-b border-gray-200">
             <div class="text-lg">Legal! sua recompensa esta garantida 😎</div>
             <small>Agora você já pode ajudar a resolver com segurança.</small>
         </div>
@@ -39,20 +36,15 @@
 
         @if ($talk->completed_at || $talk->post->completed_at)
         <div>
-            @if ($talk->post->user_id === auth()->id())
-            @isset ($talk->completed_at)
+            @if ($talk->post->user_id === auth()->id() && isset($talk->completed_at))
             <span>{{ $talk->user->name }}</span>
             <span>disse que vocês terminaram...</span>
-            @endisset
             @endif
 
-            @if ($talk->user_id === auth()->id())
-            @isset ($talk->post->completed_at)
+            @if ($talk->user_id === auth()->id() && isset($talk->post->completed_at))
             <span>{{ $talk->post->user->name }}</span>
             <span>disse que vocês terminaram...</span>
-            @endisset
             @endif
-
         </div>
         @endif
 
@@ -60,8 +52,13 @@
     </div>
     @endcan
 
+    @endif
+
     <livewire:messages-list :talk="$talk" />
 
+    @if ($talk->completed_at && $talk->post->completed_at)
+    <div class="text-center text-gray-400">Essa conversa foi finalizada!</div>
+    @else
     <livewire:messages-create-form :talk="$talk" />
     @endif
 </div>
