988ec8884970adc24b141a66834ff0689e02af36
MuriloEduardo <muriloeduardoooooo@gmail.com>
2022-08-18 17:44:11 -0300

    Break all body messages chat

--- resources/views/livewire/messages-list.blade.php
+++ resources/views/livewire/messages-list.blade.php
@@ -5,7 +5,7 @@
         <div @class([ 'inline-block	bg-white rounded p-3 overflow-hidden shadow-sm sm:rounded-lg border-b border-gray-200' , 'bg-gray-600 text-white'=> $message->user_id === auth()->id(),
             ])>
             <small class="text-gray-400">{{ $message->created_at->diffForHumans() }}</small>
-            <p>{{ $message->content }}</p>
+            <p class="break-all">{{ $message->content }}</p>
         </div>
     </div>
     @endforeach
