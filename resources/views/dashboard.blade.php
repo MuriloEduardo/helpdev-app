c404358f4e7d64c19f4cada535e7b7dbe88379f5
MuriloEduardo <muriloeduardoooooo@gmail.com>
2022-08-31 09:14:24 -0300

    Improve mobile navigation

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 border-b border-gray-200">
-                    Você está logado!
+                    Seja bem-vindo Dev! AJude alguém e seja recompensado ou peça ajuda e seja ajudado rapidamente 🤓
                 </div>
             </div>
         </div>
